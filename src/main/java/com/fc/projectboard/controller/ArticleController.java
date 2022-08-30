db3569e61e8295b14e3368947c3ed49f2f709d4a
TAEWAN KIM <bbbtw21@gmail.com>
2022-08-30 18:21:25 +0900

    게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 적용

--- src/main/java/com/fc/projectboard/controller/ArticleController.java
+++ src/main/java/com/fc/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.fc.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,5 +16,13 @@ public class ArticleController {
     String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    String article(ModelMap map, @PathVariable Long articleId) {
+        map.addAttribute("article", "article"); // TODO: 구현 시에 실제 데이터 연동
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
     }
 }
